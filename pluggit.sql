77ba3b9842afad9e3650c25f1434d78391818a14
Christian Heine <christian.heine@hsh-berlin.com>
2019-06-24 15:30:03 +0200

    MySQL: Enum instead of Set
    Use data type ENUM instead of SET, since the status can only be one state (Enum) and not several states (Set) at a time.
    (see also: https://dev.mysql.com/doc/refman/8.0/en/constraint-enum.html)

--- pluggit.sql
+++ pluggit.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `datapoints` (
   `fan1` float NOT NULL COMMENT 'fan1 rpm',
   `fan2` float NOT NULL COMMENT 'fan2 rpm',
   `humidity` smallint(6) NOT NULL,
-  `bypass` set('closed','in process','closing','opening','opened') NOT NULL,
+  `bypass` enum('closed','in process','closing','opening','opened') NOT NULL,
   `speed` tinyint(4) NOT NULL,
   PRIMARY KEY (`device`, `timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -27,6 +27,6 @@ CREATE TABLE IF NOT EXISTS `devices` (
 CREATE TABLE IF NOT EXISTS `states` (
   `device` bigint(20) NOT NULL,
   `timestamp` bigint(20) NOT NULL,
-  `state` set('Standby','Manual','Demand','Week program','Servo-flow','Away','Summer','DI Override','Hygrostat override','Fireplace','Installer','Fail Safe 1','Fail Safe 2','Fail Off','Defrost Off','Defrost','Night') NOT NULL,
-  `alarm` set('None','Exhaust FAN Alarm','Supply FAN Alarm','Bypass Alarm','T1 Alarm','T2 Alarm','T3 Alarm','T4 Alarm','T5 Alarm','RH Alarm','Outdoor13 Alarm','Supply5 Alarm','Fire Alarm','Communication Alarm','FireTermostat Alarm','VOC Alarm') NOT NULL
+  `state` enum('Standby','Manual','Demand','Week program','Servo-flow','Away','Summer','DI Override','Hygrostat override','Fireplace','Installer','Fail Safe 1','Fail Safe 2','Fail Off','Defrost Off','Defrost','Night') NOT NULL,
+  `alarm` enum('None','Exhaust FAN Alarm','Supply FAN Alarm','Bypass Alarm','T1 Alarm','T2 Alarm','T3 Alarm','T4 Alarm','T5 Alarm','RH Alarm','Outdoor13 Alarm','Supply5 Alarm','Fire Alarm','Communication Alarm','FireTermostat Alarm','VOC Alarm') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
